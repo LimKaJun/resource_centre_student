93117e2dfdb4c38bcf32e4f92ad45329b6574250
20033824 <20033824@myrp.edu.sg>
2021-07-22 13:00:13 +0800

    aloy

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -106,15 +106,14 @@ public class ResourceCentreTest {
 		// Given an empty list, after adding 2 items, test if the size of the list is 2
 		ResourceCentre.addChromebook(chromebookList, cb1);
 		ResourceCentre.addChromebook(chromebookList, cb2);
-		assertEquals("Test if that Camcorder arraylist size is 2?", 2, camcorderList.size());
+		assertEquals("Test if that Camcorder arraylist size is 2?", 2, chromebookList.size());
 
-		// test if the expected output string same as the list of camcorders retrieved
+		// test if the expected output string same as the list of chromebook retrieved
 		// from the SourceCentre
 		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
 
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0011", "Nikon HDSLR", "Yes", "", 40);
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
-
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
 		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 	}
 
